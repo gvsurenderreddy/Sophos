a97b6d489bdd8ae8c331fae320f94520b0e1d3c6
Vasanth Narayanan <vasani@Vasanths-iMac.attlocal.net>
2015-06-30 12:31:40 -0700

    ui changes

--- SAT/app/view/main/MainController.js
+++ SAT/app/view/main/MainController.js
@@ -52,6 +52,102 @@ Ext.define('SAT.view.main.MainController', {
         });
     },
 
+    handleCheckBoxSelection: function(checkbox, checked) {
+
+    },
+
+    onThreatClick: function(data) {
+
+    },
+
+    onThreatExpandBody: function() {
+
+    },
+
+    onThreatCollapseBody: function() {
+
+    },
+
+    renderGridIcons: function(value, metaData) {
+        var data = {
+            value: value
+        };
+        var tpl = new Ext.XTemplate("<div class='gridIcons'><img src='resources/images/{value}'></div>");
+        var html = tpl.apply(data);
+        return html;
+    },
+
+    renderGridContent: function(value, metaData) {
+        var title = metaData.record.data.threatTitle,
+            data = {
+                image: metaData.record.data.gridIcon,
+                title: title,
+                value: value
+            };
+        if(title === 'Internet Speed Test') {
+            var tpl = new Ext.XTemplate(
+                "<div class='gridContent'>" +
+                    "<div class='top-div'>"+
+                        "<div class='gridSmallIcon' style='float: left'><img src='resources/images/{imagee}'></div>" +
+                        "<div class='threatTitle' style='float: left; margin-left: 5px; color: #0171B9; font-size: medium; font-weight: 400'>{title}</div>&nbsp;&nbsp;"+
+                    "</div>"+
+
+                    "<table class='stats>" +
+                        "<tr class='stats-row'>" +
+                            "<td class='ping'>" +
+                                "<p style='margin-top: 10px; color: #0171B9; font-size: smaller; font-weight: 600;' class='ping-text'>Ping</p>"+
+                                "<div class='ping-box' style='margin-top:-10px; background-color:#C3E2E6; text-align: center; color:black; width:90px; height: 34px; font-size: 18px; font-weight: 300; padding-top: 10px'>10ms</div>"+
+                            "</td>"+
+                            "<td class='download'>" +
+                                "<p style='margin-top: 10px; color: #0171B9; font-size: smaller; font-weight: 600;' class='ping-text'>Download Speed</p>"+
+                                "<div class='ping-box' style='margin-top:-10px; background-color:#C3E2E6; text-align: center; color:black; width:90px; height: 34px; font-size: 18px; font-weight: 300; padding-top: 10px'>45.2Mbps</div>"+
+                            "</td>"+
+                            "<td class='upload'>" +
+                                "<p style='margin-top: 10px; color: #0171B9; font-size: smaller; font-weight: 600;' class='ping-text'>Upload Speed</p>"+
+                                "<div class='ping-box' style='margin-top:-10px; background-color:#C3E2E6; text-align: center; color:black; width:90px; height: 34px; font-size: 18px; font-weight: 300; padding-top: 10px'>15.9Mbps</div>"+
+                            "</td>"+
+                            "<td style='padding-left: 10px' class='upload'>" +
+                                "<div class='gridContent' style='white-space: pre-wrap;font-family: Flama-Basic;font-weight: 400; float: left'>{value}</div>"+
+                                "<a href='#details' style='float: right; font-family: Flama-Basic; font-weight: 300; text-decoration: none; color:#0171B9;'>View Details</a>"+
+                            "</td>"+
+                        "</tr>"+
+                    "</table>"+
+
+                    //"<div class='mid-div'>"+
+                    //    "<div class='gridContent' style='white-space: pre-wrap;font-family: Flama-Basic;font-weight: 400; float: left'>{value}</div>"+
+                    //"</div>"+
+                "</div>");
+            var html = tpl.apply(data);
+
+            return html;
+
+        } else {
+            var tpl = new Ext.XTemplate(
+                "<div class='gridContent'>" +
+                    "<div class='top-div'>"+
+                        "<div class='gridSmallIcon' style='float: left'><img src='resources/images/{imagee}'></div>" +
+                        "<div class='threatTitle' style='float: left; margin-left: 5px; color: #0171B9; font-size: medium; font-weight: 400'>{title}</div>&nbsp;&nbsp;"+
+
+                        "<div class='result-pass' style='float: left; margin-left: 5px; background-color:rgb(22, 192, 81); text-align: center; color:#f5f5f5; width:80px; height: 18px;'>Passed!</div>"+
+                        "<div class='result-fail' style='float: left; margin-left: 5px; background-color:#cc0000; text-align: center; color:#f5f5f5; width:80px; height: 18px; display: none'>Failed!</div>"+
+                    "</div>"+
+
+                    "<div class='mid-div'>"+
+                        "<div class='gridContent' style='white-space: pre-wrap;font-family: Flama-Basic;font-weight: 400; float: left'>{value}</div>"+
+                    "</div>"+
+
+                    "<br><br>"+
+                    "<div class='bottom-div' style='margin-top: 25px;'>"+
+                        "<a href='#details' style='float: right; font-family: Flama-Basic; font-weight: 300; text-decoration: none; color:#0171B9;'>View Details</a>"+
+                        //"<input type='button' value='Rerun Test' style='margin-right: 10px; background: #007ac6; float:right; border-radius: 5px; width: 80px; color: #fff; font-size: 10px;'>"+
+                    "</div>"+
+                "</div>");
+            var html = tpl.apply(data);
+            return html;
+        }
+        //var tpl = new Ext.XTemplate("<div class='threatTitle' style='float: left; margin-left: 5px; color: #0171B9; font-size: medium; font-weight: 400'>{title}</div><br><div class='gridContent'  style='white-space: pre-wrap;font-family: Flama-Basic;font-weight: 400'>{value}</div>");
+    },
+
     onClickStartFullAudit: function(a, b) {
         var me = this,
             arr = ['http://updates.html5rocks.com/', 'http://www.simplyhired.com/'];
