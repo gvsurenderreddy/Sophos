42ed1e2513fa51f29619a19a7bb90936f8c40f4f
vny <2005.vasanth@gmail.com>
2015-07-01 15:14:19 -0700

    ui-changes3

--- SAT/app/view/main/Main.js
+++ SAT/app/view/main/Main.js
@@ -82,7 +82,10 @@ Ext.define('SAT.view.main.Main', {
                     },
                     width: 780,
                     height: 80,
-                    text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+                    text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+                    style: {
+                        'white-space': 'pre-wrap'
+                    }
                 }]
             },
             {
