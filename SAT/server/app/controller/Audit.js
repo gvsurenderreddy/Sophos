a340225c94333bcc5909632e8c2a7fdab05183c1
vny <2005.vasanth@gmail.com>
2015-09-08 10:42:42 -0700

    checkin latest backend changes

--- SAT/server/app/controller/Audit.js
+++ SAT/server/app/controller/Audit.js
@@ -18,7 +18,197 @@ var gCheckMethods = [
 	"IMAGE"
 ];
 
+var RESULT_SUCCESS = {
+	msg: 'success'
+};
+
+var RESULT_FAILURE = {
+	msg: 'error'
+};
+
+// init the database connection
+var gMyDatabase = mongoskin.db('mongodb://@localhost:27017/sat', {safe:true});
+var ID_HELPER = mongoskin.helper.toObjectID;
+
 var Audit = {
+
+	handleCollName: function(req, res, next, collName) {
+		req.collName = collName;
+		req.table = gMyDatabase.collection(collName);
+		return next();
+	},
+
+	tableList: function(req, res, next) {
+		var opts = {};
+
+		if (req.query.limit) {
+			var myLimit = parseInt(req.query.limit, 10);
+			opts = extend(opts, { limit: myLimit });
+		}
+
+		if (req.query.skip) {
+			var mySkip = parseInt(req.query.skip, 10);
+			opts = extend(opts, { skip: mySkip });
+		}
+
+		if (req.query.sort) {
+			var mySortArr = [];
+			var myMainSort = [];
+			myMainSort.push(req.query.sort);
+
+			if (req.query.desc == 'true') {
+				myMainSort.push('desc');
+			}
+			else if (req.query.asc == 'true') {
+				myMainSort.push('asc');
+			}
+			else {
+				myMainSort.push('asc');
+			}
+
+			mySortArr.push(myMainSort);
+		
+			opts = extend(opts, { sort: mySortArr });
+		}
+
+		// get the collection name
+		var myCollName = req.collName;
+
+		// set to default
+		var selector = {};
+
+		// filters for url collection
+		if (myCollName == 'url') {
+			if (req.query.audit_type && !isNaN(req.query.audit_type)) {
+				var auditTypeSelector = { audit_type: parseInt(req.query.audit_type) };
+
+				if (!selector.$and) {
+					selector.$and = [];
+				}
+				selector.$and.push(auditTypeSelector);
+			}
+
+			if (req.query.gid && !isNaN(req.query.gid)) {
+				var gidSelector = { gid: parseInt(req.query.gid) };
+
+				if (!selector.$and) {
+					selector.$and = [];
+				}
+				selector.$and.push(gidSelector);
+			}
+
+			if (req.query.jsonp == 'true') {
+				var jsonpSelector = { $or: [{ jsonp: true }, { jsonp: "true" }] };
+
+				if (!selector.$and) {
+					selector.$and = [];
+				}
+				selector.$and.push(jsonpSelector);
+			}
+
+			if (req.query.cors == 'true') {
+				var corsSelector = { $or: [{ cors: true }, { cors: "true" }] };
+
+				if (!selector.$and) {
+					selector.$and = [];
+				}
+				selector.$and.push(corsSelector);
+			}
+		}
+	
+		req.table.find(
+			selector,
+			opts
+		).toArray(function(e, results){
+				if (e) { 
+					return next(e);
+				}
+			
+				res.send(results);
+			}
+		)
+	},
+
+	tableItemById: function(req, res, next) {
+		req.table.findOne(
+			{
+				_id: ID_HELPER(req.params.id)
+			}, 
+			function(e, result){
+				if (e) {
+					return next(e);
+				}
+			
+				res.send(result);
+			}
+		)
+	},
+
+	tableAddMultiple: function(req, res, next) {
+		console.log(req.body);
+		req.table.insert(
+			req.body, 
+			{}, 
+			function(e, results){
+				if (e) {
+					return next(e);
+				}
+			
+				res.send(results);
+			}
+		);
+	},
+
+	tableEditSingle: function(req, res, next) {
+		req.table.update(
+			{
+				_id: ID_HELPER(req.params.id)
+			},
+			{
+				$set: req.body
+			},
+			{
+				safe: true, 
+				multi: false
+			}, 
+			function(e, result){
+				if (e) {
+					return next(e);
+				}
+			
+				res.send((result === 1) ? RESULT_SUCCESS : RESULT_FAILURE);
+			}
+		);
+	},
+
+	tableDeleteSingle: function(req, res, next) {
+		req.table.remove(
+			{
+				_id: ID_HELPER(req.params.id)
+			}, 
+			function(e, result){
+				if (e) {
+					return next(e);
+				}
+					
+				res.send((result === 1) ? RESULT_SUCCESS : RESULT_FAILURE);
+			}
+		);
+	},
+
+	tableDeleteAll: function(req, res, next) {
+		req.table.remove(
+			{}, 
+			function(e, result){
+				if (e) {
+					return next(e);
+				}
+				
+				res.send(RESULT_SUCCESS);
+			}
+		);
+	},
+
     getActiveURLS: function(req, res) {
 	var opts = {};
 
--- SAT/server/app/router.js
+++ SAT/server/app/router.js
@@ -1,8 +1,21 @@
-var audit = require('./controller/Audit');
+var audit = require('./controller/Audit'),
+	bodyParser = require('body-parser');
 
 var router = {
 
     init: function(app) {
+
+		// generic table/collection REST handlers
+		app.use(bodyParser.urlencoded({ extended: true }));
+		app.use(bodyParser.json());
+		app.param('collName', audit.handleCollName);
+		app.get('/rxapi/:collName', audit.tableList);
+		app.get('/rxapi/:collName/:id', audit.tableItemById);
+		app.post('/rxapi/:collName', audit.tableAddMultiple);
+		app.put('/rxapi/:collName/:id', audit.tableEditSingle);
+		app.delete('/rxapi/:collName/:id', audit.tableDeleteSingle);
+		app.delete('/rxapi/:collName', audit.tableDeleteAll);
+
         app.get('/urlActive', audit.getActiveURLS);
         app.get('/urlList', audit.getBotURLS);
         app.get('/urlSSLList', audit.getSSLList);
